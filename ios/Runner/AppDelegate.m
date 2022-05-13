3775ffc9ccef0c1c841a665c861fd7276a54e18d
João Victor Ramalho Pereira <victorjudoca2009@hotmail.com>
2022-05-13 10:38:14 -0300

    first commit

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,15 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCIYwa7FPxtnRR596aDRllp5RDhsMQQ93M"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
